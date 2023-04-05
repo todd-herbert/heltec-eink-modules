6e951589d2250e4bff3e98e97ad2c0ff1d1c759c
Todd Herbert <herbert.todd@gmail.com>
2023-04-05 21:08:03 +1200

    chore: purge HTE029A1 code

--- src/heltec-eink-modules.h
+++ src/heltec-eink-modules.h
@@ -4,4 +4,3 @@
 #include "Displays/DEPG0150BNS810/DEPG0150BNS810.h"                     //Heltec 1.54" BW V2    -   Red Tab
 #include "Displays/QYEG0213RWS800/QYEG0213RWS800.h"                     //Heltec 2.13" RED V2   -   Red Tab
 #include "Displays/DEPG0290BNS75A/DEPG0290BNS75A.h"                     //Heltec 2.9" BW V2     -   Red Tab
-#include "Displays/HTE029A1/HTE029A1.h"                               //Heltec 2.9" BW V2     -   Blue Tab
