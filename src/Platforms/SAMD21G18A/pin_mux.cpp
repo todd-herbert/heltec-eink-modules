982f773f56a4ec98a0f60634818fc0694fcea585
Todd Herbert <herbert.todd@gmail.com>
2023-10-05 22:15:31 +1300

    refactor: rename pin mux terminology

--- src/Platforms/SAMD21G18A/pin_mux.cpp
+++ src/Platforms/SAMD21G18A/pin_mux.cpp
@@ -8,102 +8,104 @@
 
 #include "pin_mux.h"
 
-// Find and set SDI and CLK pin muxings for SAMD21 SPI. Returns the automatic MISO pin selection
+// Find and set SDI and CLK pin muxing for SAMD21 SPI. Returns the automatic MISO pin selection (sercom assigned, but pinmode not set)
 uint8_t PinMux::setSPIPins(uint8_t sdi, uint8_t clk) {
     // Pass down
     return setSPIPins(sdi, clk, -1);
 }
 
-// Find and set a pin muxing for SAMD21 SPI. Returns the selected MISO pin, incase it was auto-assigned
+// Find and set pin muxing for SAMD21 SPI. Returns the selected MISO pin, incase it was auto-assigned. (assigned, but not enabled)
 uint8_t PinMux::setSPIPins(uint8_t sdi, uint8_t clk, uint8_t miso) {
 
-    // For every valid mux
-    for( uint8_t s = 0; s < sizeof(valid_muxs) / sizeof(Mux); s++ ) {
+    // For every option in list of available pin configs
+    for( uint8_t s = 0; s < sizeof(available_options) / sizeof(MuxOption); s++ ) {
 
-        // Try all mux options
-        Mux sdi_mux = valid_muxs[s];
+        // Inspect as potential SDI config
+        MuxOption sdi_conf = available_options[s];
     
         // Wrong SDI pin, not considered
-        if (sdi_mux.pin != sdi)
+        if (sdi_conf.pin != sdi)
             continue;
 
-        // Re-check aganst the mux, array, looking for clk
-        for( uint8_t c = 0; c < sizeof(valid_muxs) / sizeof(Mux); c++ ) {
-            Mux clk_mux = valid_muxs[c];
+        // Re-check aganst the options list: find a suitable CLK option
+        for( uint8_t c = 0; c < sizeof(available_options) / sizeof(MuxOption); c++ ) {
 
-            // If wrong clk pin, not considered
-            if (clk_mux.pin != clk)
+            // Inspect as potential CLK config
+            MuxOption clk_conf = available_options[c];
+
+            // CLK pin number wrong, not considered
+            if (clk_conf.pin != clk)
                 continue;
 
-            // Non-matching sercom, not considered
-            if (sdi_mux.sercom != clk_mux.sercom)
+            // CLK option uses different sercom to SDI option, not considered
+            if (sdi_conf.sercom != clk_conf.sercom)
                 continue;
 
             // Duplicate entry, not considered (re: ICSP pins)
-            if (sdi_mux.pad == clk_mux.pad)
+            if (sdi_conf.pad == clk_conf.pad)
                 continue;
 
-            // Both pins exist on the same sercom
+            // Both pin options use the same sercom
             // Now, check if the pads will work
 
             SercomSpiTXPad tx_pad;
             SercomRXPad rx_pad;
 
-            if (sdi_mux.pad == 0 && clk_mux.pad == 1) 
+            if (sdi_conf.pad == 0 && clk_conf.pad == 1) 
                 tx_pad = SPI_PAD_0_SCK_1; 
 
-            else if(sdi_mux.pad == 2 && clk_mux.pad == 3)
+            else if(sdi_conf.pad == 2 && clk_conf.pad == 3)
                 tx_pad = SPI_PAD_2_SCK_3;
 
-            else if(sdi_mux.pad == 3 && clk_mux.pad == 1)
+            else if(sdi_conf.pad == 3 && clk_conf.pad == 1)
                 tx_pad = SPI_PAD_3_SCK_1;
 
-            else if(sdi_mux.pad == 0 && clk_mux.pad == 3)
+            else if(sdi_conf.pad == 0 && clk_conf.pad == 3)
                 tx_pad = SPI_PAD_0_SCK_3;
 
-            else    // No suitable pad setting
+            else    // No suitable pad setting (for user resquested pins)
                 continue;
 
+            // Check list of options for a valid MISO config
+            for( uint8_t m = 0; m < sizeof(available_options) / sizeof(MuxOption); m++ ) {
 
+                // Inspect as potential MISO config
+                MuxOption miso_conf = available_options[m];
 
-            // Lets go hunting for a MISO muxing
-            for( uint8_t m = 0; m < sizeof(valid_muxs) / sizeof(Mux); m++ ) {
-                Mux miso_mux = valid_muxs[m];
-
-                // If use has specified miso, and this doesn't match: not considered
-                if (miso != 0xFF && miso_mux.pin != miso)
+                // If user specified a MISO pin, and this doesn't match: not considered
+                if (miso != 0xFF && miso_conf.pin != miso)
                     continue;
 
-                // Non-matching sercom, not considered
-                if (miso_mux.sercom != clk_mux.sercom)
+                // Different SERCOM to SDI and CLK, not considered
+                if (miso_conf.sercom != clk_conf.sercom)
                     continue;
                 
                 // Reject if it's a duplicate (A1 - ICSP MOSI)
-                if (miso_mux.pad == sdi_mux.pad || miso_mux.pad == clk_mux.pad)
+                if (miso_conf.pad == sdi_conf.pad || miso_conf.pad == clk_conf.pad)
                     continue;
 
                 // Set the RX pad, for the SPI constructor
-                rx_pad = (SercomRXPad)miso_mux.pad;
+                rx_pad = (SercomRXPad)miso_conf.pad;
 
 
                 // We have all three pins, time to reconfigure SPI
-                SPI = SPIClassSAMD(clk_mux.sercom, miso, clk, sdi, tx_pad, rx_pad);
-                pinPeripheral(sdi, sdi_mux.type);
-                pinPeripheral(clk, clk_mux.type);
+                SPI = SPIClassSAMD(clk_conf.sercom, miso, clk, sdi, tx_pad, rx_pad);
+                pinPeripheral(sdi, sdi_conf.type);
+                pinPeripheral(clk, clk_conf.type);
 
                 // Also set MISO, if the user requested a pin for it
                 if (miso != 0xFF) {
                     pinPeripheral(MISO, PIO_INPUT); // Unset original MISO pin - seems necessary
-                    pinPeripheral(miso_mux.pin, miso_mux.type);
+                    pinPeripheral(miso_conf.pin, miso_conf.type);
                 }
 
-                return miso_mux.pin;    // Pass miso back to BaseDisplay, incase it was auto-assigned here 
+                return miso_conf.pin;    // Pass miso back to BaseDisplay, incase it was auto-assigned here (unnecessary?)
 
-            } // End of loop - find miso
+            }   // End of loop - find MISO
 
-        }   // End of loop- find matching clk
+        }   // End of loop - find CLK
 
-    }   // End of loop - find sdi pin
+    }   // End of loop - find SDI
 
     // SPI pinout not possible - Blink SOS on LED_BUILTIN
     SOS();
--- src/Platforms/SAMD21G18A/pin_mux.h
+++ src/Platforms/SAMD21G18A/pin_mux.h
@@ -22,31 +22,31 @@ class PinMux {
         // A call for help
         void SOS();
 
-        // A possible valid mux
-        struct Mux {
+        // A valid, possible MUX option
+        struct MuxOption {
             uint8_t pin;
             SERCOM* sercom;
             uint8_t pad;
             EPioType type;
         };
         
-        // Array of all relevant mux options, for all relevant pins
-        const Mux valid_muxs[15] = {
-            (Mux) {2, &sercom2, 2, PIO_SERCOM},         // D2,          sercom2/pad[2]
-            (Mux) {2, &sercom4, 2, PIO_SERCOM_ALT},     // D2,      alt_sercom4/pad[2]
-            (Mux) {3, &sercom2, 1, PIO_SERCOM_ALT},     // D3,      alt_sercom2/pad[1]  
-            (Mux) {4, &sercom2, 0, PIO_SERCOM_ALT},     // D4,      alt_sercom2/pad[0]
-            (Mux) {5, &sercom2, 3, PIO_SERCOM},         // D5,          sercom2/pad[3]
-            (Mux) {5, &sercom4, 3, PIO_SERCOM_ALT},     // D5,      alt_sercom4/pad[3]
-            (Mux) {10, &sercom1, 2, PIO_SERCOM},        // D10,         sercom1/pad[2]
-            (Mux) {11, &sercom1, 0, PIO_SERCOM},        // D11,         sercom1/pad[0]
-            (Mux) {12, &sercom1, 3, PIO_SERCOM},        // D12,         sercom1/pad[3]
-            (Mux) {13, &sercom1, 1, PIO_SERCOM},        // D13,         sercom1/pad[1]
-            (Mux) {A1, &sercom4, 0, PIO_SERCOM_ALT},    // A1,          sercom4/pad[0]
-            (Mux) {A2, &sercom4, 1, PIO_SERCOM_ALT},    // A2,          sercom4/pad[1]
-            (Mux) {MISO, &sercom4, 0, PIO_SERCOM_ALT},  // ICSP MISO,   sercom4/pad[0]  - duplicate
-            (Mux) {MOSI, &sercom4, 2, PIO_SERCOM_ALT},  // ICSP MOSI,   sercom4/pad[2]
-            (Mux) {SCK, &sercom4, 3, PIO_SERCOM_ALT}    // ICSP SCK,    sercom4/pad[3]
+        // Array of all relevant muxing options, for all relevant pins
+        const MuxOption available_options[15] = {
+            (MuxOption) {2, &sercom2, 2, PIO_SERCOM},           // D2,          sercom2/pad[2]
+            (MuxOption) {2, &sercom4, 2, PIO_SERCOM_ALT},       // D2,      alt_sercom4/pad[2]
+            (MuxOption) {3, &sercom2, 1, PIO_SERCOM_ALT},       // D3,      alt_sercom2/pad[1]  
+            (MuxOption) {4, &sercom2, 0, PIO_SERCOM_ALT},       // D4,      alt_sercom2/pad[0]
+            (MuxOption) {5, &sercom2, 3, PIO_SERCOM},           // D5,          sercom2/pad[3]
+            (MuxOption) {5, &sercom4, 3, PIO_SERCOM_ALT},       // D5,      alt_sercom4/pad[3]
+            (MuxOption) {10, &sercom1, 2, PIO_SERCOM},          // D10,         sercom1/pad[2]
+            (MuxOption) {11, &sercom1, 0, PIO_SERCOM},          // D11,         sercom1/pad[0]
+            (MuxOption) {12, &sercom1, 3, PIO_SERCOM},          // D12,         sercom1/pad[3]
+            (MuxOption) {13, &sercom1, 1, PIO_SERCOM},          // D13,         sercom1/pad[1]
+            (MuxOption) {A1, &sercom4, 0, PIO_SERCOM_ALT},      // A1,          sercom4/pad[0]
+            (MuxOption) {A2, &sercom4, 1, PIO_SERCOM_ALT},      // A2,          sercom4/pad[1]
+            (MuxOption) {MISO, &sercom4, 0, PIO_SERCOM_ALT},    // ICSP MISO,   sercom4/pad[0]  - duplicate
+            (MuxOption) {MOSI, &sercom4, 2, PIO_SERCOM_ALT},    // ICSP MOSI,   sercom4/pad[2]
+            (MuxOption) {SCK, &sercom4, 3, PIO_SERCOM_ALT}      // ICSP SCK,    sercom4/pad[3]
         };
 };
 
