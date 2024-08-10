d44f1c6336495035501ae408f822fcb2e265a3ff
Todd Herbert <herbert.todd@gmail.com>
2024-08-10 23:34:51 +1200

    Soften fast-mode for DEPG0290BNS800 Use same refresh operation as Meshtastic for HT-VME290

--- src/Displays/DEPG0290BNS800/mode.cpp
+++ src/Displays/DEPG0290BNS800/mode.cpp
@@ -4,8 +4,20 @@
 PROGMEM constexpr uint8_t DEPG0290BNS800::lut_partial[];
 
 void DEPG0290BNS800::configPartial() {
+    // Border waveform:
+    // Actively hold the edge of the display white during update
+    sendCommand(0x3C);
+    sendData(0x60);
 
-    // Load the fastmode lut
+    // Source driving voltage:
+    // Manufacturer's datasheet lists +/-15V as "typical" voltages. I suspect the full refresh voltages from OTP are slightly higher.
+    // The refresh operation is slightly weak at these voltages, but seems much healthier for the panel.
+    sendCommand(0x04);
+    sendData(0x41); // VSH1 15V
+    sendData(0x00); // VSH2 NA
+    sendData(0x32); // VSL -15V
+
+    // Send custom LUT for partial refresh
     sendCommand(0x32);
     for(uint8_t i=0;i < sizeof(lut_partial); i++) 
         sendData(pgm_read_byte_near(lut_partial+i));
